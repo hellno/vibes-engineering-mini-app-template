fe079ce447944715d71c82095cd898aac489a81b
hellno <hellno@users.noreply.github.com>
2025-01-09 17:56:09 +0100

    wip

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
-
-export const PROJECT_ID = uuidv4();
+export const PROJECT_ID = 'farcaster-frames-template';
 export const PROJECT_TITLE = "Farcaster Frames Template";
 export const PROJECT_DESCRIPTION = "A Farcaster Frames v2 Template by hellno";
