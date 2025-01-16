d54ba812e28de75f805bd850824896ecb8014e37
hellno <hellno@users.noreply.github.com>
2025-01-16 10:29:58 +0100

    re-use constant in .well-known

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,3 +1,5 @@
+import { PROJECT_TITLE } from "~/lib/constants";
+
 export async function GET() {
   const appUrl = process.env.NEXT_PUBLIC_URL || `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
@@ -11,7 +13,7 @@ export async function GET() {
     },
     frame: {
       version: "1",
-      name: "Frames v2 Demo",
+      name: {PROJECT_TITLE},
       iconUrl: `${appUrl}/icon.png`,
       homeUrl: appUrl,
       imageUrl: `${appUrl}/frames/hello/opengraph-image`,
