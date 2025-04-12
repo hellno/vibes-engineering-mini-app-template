72ab5b592a9ba059790c18f10db5ddc169f24e25
Gotlop <haitayo750@gmail.com>
2025-04-12 18:09:42 +0700

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const PROJECT_ID = 'farcaster-frames-template';
-export const PROJECT_TITLE = "Farcaster Frames Template";
-export const PROJECT_DESCRIPTION = "A Farcaster Frames v2 Template by hellno";
+export const PROJECT_TITLE = "Blepblopia Translator";
+export const PROJECT_DESCRIPTION = "Translate your text into Blepblopia language.";
