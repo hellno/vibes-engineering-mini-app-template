e25404929babf70542e5650303c3c87a4a2ac0ce
hellno <hellno@users.noreply.github.com>
2025-07-25 16:37:00 +0200

    add celo in wagmi provider

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -1,15 +1,24 @@
 import { createConfig, http, injected, WagmiProvider } from "wagmi";
-import { base, degen, mainnet, optimism } from "wagmi/chains";
+import { base, celo, degen, mainnet, optimism } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
 import { DaimoPayProvider, getDefaultConfig } from "@daimo/pay";
 import { PROJECT_TITLE } from "~/lib/constants";
 
+const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+
 export const config = createConfig(
   getDefaultConfig({
     appName: PROJECT_TITLE,
-    chains: [base, degen, mainnet, optimism],
+    chains: [base, degen, mainnet, optimism, celo],
     additionalConnectors: [farcasterMiniApp(), injected()],
+    transports: {
+      [base.id]: http(
+        alchemyApiKey
+          ? `https://base-mainnet.g.alchemy.com/v2/${alchemyApiKey}`
+          : undefined,
+      ),
+    },
   }),
 );
 
